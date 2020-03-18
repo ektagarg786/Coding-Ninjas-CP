0b6839fca77023db24d888f1ec9638672cc37715
ektagarg786 <60681475+ektagarg786@users.noreply.github.com>
2020-03-18 23:19:09 +0530

    Create Even_Odd_Indexes.cpp

---
+++ Prerequisites/Even_Odd_Indexes.cpp
@@ -0,0 +1,44 @@
+/*Given an array of integers, print two integer values:
+First, the sum of all numbers which are even as well as whose index are even.
+Second, the sum of all numbers which are odd as well as whose index are odd.
+Print the two integers space separated. (Arrays is 0-indexed)
+Input:
+Given an integer denoting the size of array.
+Next line will have a line containing ‘n’ space separated integers.
+Constraints:
+1<=n<=10^5
+1 <= Ai <= 10^6 
+Output:
+Two space separated integers denoting even and odd sums respectively.
+Sample Input:
+5
+2 3 5 1 4
+Sample Output:
+6 4*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    int sum1 = 0, sum2 = 0;
+    for(int i=0;i<n;i++)
+    {
+        if(i%2 == 0 && arr[i] % 2 == 0)
+        {
+            sum1 += arr[i];
+        }
+        else if(i%2 !=0 && arr[i] % 2 != 0)
+        {
+            sum2 += arr[i];
+        }
+    }
+    cout << sum1 << " " << sum2 << endl;
+	return 0;
+}
