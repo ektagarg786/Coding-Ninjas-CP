a4527d0227e9e4ba597b837027faab6ced50e700
ektagarg786 <60681475+ektagarg786@users.noreply.github.com>
2020-03-18 23:38:30 +0530

    Create Tell_The_Positions.cpp

---
+++ Language_tools/Tell_The_Positions.cpp
@@ -0,0 +1,53 @@
+/*In a class there are ‘n’ number of students. They have three different subjects: Data Structures, Algorithm Design & Analysis and Operating Systems. Marks for each subject of all the students are provided to you. You have to tell the position of each student in the class. Print the names of each student according to their position in class. Tie is broken on the basis of their roll numbers. Between two students having same marks, the one with less roll number will have higher rank. The input is provided in order of roll number.
+Input Format:
+First line will have a single integer ‘n’, denoting the number of students in the class.
+Next ‘n’ lines each will have one string denoting the name of student and three space separated integers m1, m2, m3 denoting the marks in three subjects.
+Output Format:
+Print ‘n’ lines having two values: First, the position of student in the class and second his name.
+Constraints:
+1 <= n <= 10^5
+0 <= m1, m2, m3 <= 100
+Sample Input:
+3
+Mohit 94 85 97
+Shubham 93 91 94
+Rishabh 95 81 99
+Sample Output:
+1 Shubham
+2 Mohit
+3 Rishabh*/
+
+#include<bits/stdc++.h>
+using namespace std;
+struct rank{
+    int total;
+    string name;
+    int rollno;
+};
+bool compare(rank r1,rank r2){
+	if(r1.total == r2.total){
+		return (r1.rollno<r2.rollno);
+	}
+	else{
+		return (r1.total>r2.total);
+	}
+}
+int main(){
+    int n;
+    cin>>n;
+   	struct rank r[n];
+   	int roll=1;
+    for(int i=0;i<n;i++){
+         int n1,n2,n3;
+         cin>>r[i].name;
+         cin>>n1>>n2>>n3;
+         r[i].total = n1+n2+n3;
+         r[i].rollno=roll++;
+     }  
+     sort(r,r+n,compare);
+     int k=1;
+     for(int i=0;i<n;i++){
+         cout<<k++<<" "<<r[i].name<<endl;
+    }
+	return 0;
+}
