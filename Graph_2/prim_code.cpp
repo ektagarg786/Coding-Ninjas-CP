dfd1a200ea0c7bdef7880f6ca70794da5c1eb2f8
ektagarg786 <60681475+ektagarg786@users.noreply.github.com>
2020-03-30 21:22:47 +0530

    Create prim_code.cpp

---
+++ Graph_2/prim_code.cpp
@@ -0,0 +1,95 @@
+/*Given an undirected, connected and weighted graph G(V, E) with V number of vertices (which are numbered from 0 to V-1) and E number of edges.
+Find and print the Minimum Spanning Tree (MST) using Prim's algorithm.
+For printing MST follow the steps -
+1. In one line, print an edge which is part of MST in the format -
+v1 v2 w
+where, v1 and v2 are the vertices of the edge which is included in MST and whose weight is w. And v1 <= v2 i.e. print the smaller vertex first while printing an edge.
+2. Print V-1 edges in above format in different lines.
+Note : Order of different edges doesn't matter.
+Input Format :
+Line 1: Two Integers V and E (separated by space)
+Next E lines : Three integers ei, ej and wi, denoting that there exists an edge between vertex ei and vertex ej with weight wi (separated by space)
+Output Format :
+MST
+Constraints :
+2 <= V, E <= 10^5
+Sample Input 1 :
+4 4
+0 1 3
+0 3 5
+1 2 1
+2 3 8
+Sample Output 1 :
+0 1 3
+1 2 1
+0 3 5*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int get_min_vertex(int* weight, int v, bool* visited){
+    int min_vertex = -1;
+    for(int i=0;i<v;i++){
+        if(! visited[i] && ((min_vertex == -1) || (weight[min_vertex] > weight[i])))
+            min_vertex = i;
+    }
+    return min_vertex;
+}
+void prim_mst(int** edges, int v){
+    bool* visited = new bool[v];
+    for(int i=0;i<v;i++){
+        visited[i] = false;
+    }
+    int* parent = new int[v];
+    int* weight = new int[v];
+    for(int i=0;i<v;i++){
+        weight[i] = INT_MAX;
+    }
+    weight[0] = 0;
+    parent[0] = -1;
+    for(int i=0;i<v-1;i++){
+        int min_vertex = get_min_vertex(weight, v, visited);
+        visited[min_vertex] = true;
+        for(int j=0;j<v;j++){
+            if(edges[min_vertex][j] != 0 && !visited[j]){
+                if(weight[j] > edges[min_vertex][j]){
+                    weight[j] = edges[min_vertex][j];
+                    parent[j] = min_vertex;
+                }
+            }
+        }
+    }
+    for(int i=1;i<v;i++){
+        if(parent[i] < i){
+            cout << parent[i] << " " << i << " " << weight[i] << endl;
+        }
+        else{
+            cout << i << " " << parent[i] << " " << weight[i] << endl;
+        }
+    }
+    delete [] weight;
+    delete [] parent;
+    delete [] visited;
+}
+int main(){
+    int v, e;
+    cin >> v >> e;
+    int** edges = new int*[v];
+    for(int i=0;i<v;i++){
+        edges[i] = new int[v];
+        for(int j=0;j<v;j++){
+            edges[i][j] = 0;
+        }
+    }
+    for(int i=0;i<e;i++){
+        int f, s, w;
+        cin >> f >> s >> w;
+        edges[f][s] = w;
+        edges[s][w] = w;
+    }
+    prim_mst(edges, v);
+    for(int i=0;i<v;i++){
+        delete edges[i];
+    }
+    delete [] edges;
+    return 0;
+}
