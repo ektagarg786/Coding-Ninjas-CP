d775f3f134f2335df61e46e62be858498dae7170
ektagarg786 <60681475+ektagarg786@users.noreply.github.com>
2020-03-30 20:55:44 +0530

    Create bfs_traversal.cpp

---
+++ Graph_1/bfs_traversal.cpp
@@ -0,0 +1,78 @@
+/*Given an undirected and connected graph G(V, E), print its BFS traversal.
+Here you need to consider that you need to print BFS path starting from vertex 0 only.
+V is the number of vertices present in graph G and vertices are numbered from 0 to V-1.
+E is the number of edges present in graph G.
+Note : 1. Take graph input in the adjacency matrix.
+2. Handle for Disconnected Graphs as well
+Input Format :
+Line 1: Two Integers V and E (separated by space)
+Next 'E' lines, each have two space-separated integers, 'a' and 'b', denoting that there exists an edge between Vertex 'a' and Vertex 'b'.
+Output Format :
+BFS Traversal (separated by space)
+Constraints :
+2 <= V <= 1000
+1 <= E <= 1000
+Sample Input 1:
+4 4
+0 1
+0 3
+1 2
+2 3
+Sample Output 1:
+0 1 3 2*/
+
+#include<bits/stdc++.h>
+using namespace std;
+void print_bfs(int** edges, int v, int sv, bool* visited){
+    queue<int> pendingVertices;
+    pendingVertices.push(sv);
+    visited[sv] = true;
+    while(!pendingVertices.empty()){
+        int currentVertex = pendingVertices.front();
+        pendingVertices.pop();
+        cout << currentVertex << " ";
+        for(int i=0;i<v;i++){
+            if(i == currentVertex){
+                continue;
+            }
+            if(edges[currentVertex][i] == 1 && !visited[i]){
+                pendingVertices.push(i);
+                visited[i] = true;
+            }
+        }
+    }
+}
+void bfs(int** edges, int v){
+    bool* visited = new bool[v];
+    for(int i=0;i<v;i++){
+        visited[i] = false;
+    }
+    for(int i=0;i<v;i++){
+        if(! visited[i])
+            print_bfs(edges, v, i, visited);
+    }
+    delete[] visited;
+}
+int main(){
+    int v, e;
+    cin >> v >> e;
+    int** edges = new int*[v];
+    for(int i=0;i<v;i++){
+        edges[i] = new int[v];
+        for(int j=0;j<v;j++){
+            edges[i][j] = 0;
+        }
+    }
+    for(int i=0;i<e;i++){
+        int f, s;
+        cin >> f >> s;
+        edges[f][s] = 1;
+        edges[s][f] = 1;
+    }
+    bfs(edges, v);
+    for(int i=0;i<v;i++){
+        delete edges[i];
+    }
+    delete [] edges;
+    return 0;
+}
